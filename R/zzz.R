92059d0be0127d204f502204c55738ef978e682f
Gabe Becker <gabembecker@gmail.com>
2020-01-25 13:07:19 -0800

    cran resubmission 0.14.1

--- R/zzz.R
+++ R/zzz.R
@@ -1,17 +1,24 @@
+.domessage = function(msg) message(msg)
 .onLoad = function(libname, pkgname) {
     ns = asNamespace(pkgname)
 
     delayedAssign("BiocRelease", tryCatch(BiocVers(getBiocReleaseVr()),
-                                          error = function(x)
-                                     warning("Unable to populate the BiocDevel object")),
+                                          error = function(x) {
+                                     
+                                     .domessage("Unable to populate the Bi ocRelease object")
+                                     NULL
+                                 }),
                   eval.env = ns,
                   assign.env = ns)
     delayedAssign("develVers",  getBiocDevelVr(),
                   assign.env = ns)
     
     delayedAssign("BiocDevel", tryCatch(BiocVers(getBiocDevelVr()),
-                                          error = function(x)
-                                     warning("Unable to populate the BiocDevel object")), eval.env = ns,
+                                        error = function(x) {
+                                   .domessage("Unable to populate the BiocDevel object")
+                                   NULL
+                               }
+                                   ), eval.env = ns,
                   assign.env = ns)
     delayedAssign("getBiocYaml", doyamlsetup(),
                   assign.env = ns)
